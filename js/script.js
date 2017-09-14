a601f6a2cf89f6db6a6fc935ac45fe21e169383b
dmytroTsmots <dmytro.tsmots@gmail.com>
2017-09-15 02:20:18 +0300

    add DOM-BOM HMW task 5

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var onClick = function (e) {
+	var $el = e.target;
+  
+  e.stopPropagation();
+	
+  $el.style.backgroundColor = 'yellow';
+	alert('CLICK!');
+}
+
+document.body.onclick = onClick;
+box1.onclick = onClick;
+box2.onclick = onClick;
+box3.onclick = onClick;
